ecd7bc3a2fd47fcefc3c427c6d158bbeda560c10
soshi1822 <soshi1822@gmail.com>
2022-05-11 14:53:22 +0900

    fix: 型定義を修正
    Signed-off-by: soshi1822 <soshi1822@gmail.com>

--- src/type.ts
+++ src/type.ts
@@ -2,37 +2,37 @@ import { IncomingMessageData } from './module/incoming-message';
 import { ServerResponseData } from './module/server-response';
 
 export interface ServerOption {
-    urlMaxLength?: number;
-    headerMaxLength?: number;
+  urlMaxLength?: number;
+  headerMaxLength?: number;
 }
 
 export type HttpMethods =
-    'GET'
-    | 'HEAD'
-    | 'POST'
-    | 'PUT'
-    | 'DELETE'
-    | 'CONNECT'
-    | 'OPTIONS'
-    | 'TRACE'
-    | 'PATCH'
-    | 'ALL'
-    | 'Router';
+  'GET'
+  | 'HEAD'
+  | 'POST'
+  | 'PUT'
+  | 'DELETE'
+  | 'CONNECT'
+  | 'OPTIONS'
+  | 'TRACE'
+  | 'PATCH'
+  | 'ALL'
+  | 'Router';
 
 export type RouteingParams<P> = P extends string ?
-    P extends `${infer A1}:{${infer P1}}${infer A2}` ? { [key in P1]: string } & RouteingParams<A1> & RouteingParams<A2> : {} :
-    Record<string, unknown>;
+  P extends `${infer A1}:{${infer P1}}${infer A2}` ? { [key in P1]: string } & RouteingParams<A1> & RouteingParams<A2> : {} :
+  Record<string, unknown>;
 
 
-export type RequestCallback<P = any> = (req: IncomingMessageData<P>, res: ServerResponseData) => void | Promise<void>
+export type RequestCallback<P = any, T = any> = (req: IncomingMessageData<P>, res: ServerResponseData) => T | Promise<T>;
 
 export interface RequestOption<P = any> {
-    params?: Partial<{ [key in keyof RouteingParams<P> | string]: RegExp }>
+  params?: Partial<{ [key in keyof RouteingParams<P> | string]: RegExp }>;
 }
 
 export interface RouterWaits<P> {
-    method: HttpMethods;
-    match: string | RegExp;
-    callback: RequestCallback<P>;
-    option: RequestOption<P>;
+  method: HttpMethods;
+  match: string | RegExp;
+  callback: RequestCallback<P>;
+  option: RequestOption<P>;
 }
